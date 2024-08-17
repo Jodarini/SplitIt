26452b315c8532d3ea401f2957d19bd3063bf73b
jodarini <rivasjose9428@gmail.com>
2024-08-17 17:36:48 -0500

    chore: Create bill objects, add name reactivity and render list

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import BillComp from "./components/BillComp.vue";
+
+const billItems = ref([
+  {
+    id: crypto.randomUUID(),
+    name: "Jose",
+    item: {
+      id: crypto.randomUUID(),
+      price: 3000,
+    },
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Jessica",
+    item: {
+      id: crypto.randomUUID(),
+      price: 3000,
+    },
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "Alguien",
+    item: {
+      id: crypto.randomUUID(),
+      price: 3000,
+    },
+  },
+]);
 </script>
 
 <template>
   <div class="bg-ctp-surface0/30 p-2">
     <h1 class="text-2xl font-bold w-full">SplitIt</h1>
   </div>
-
-  <BillComp />
+  <BillComp v-for="owner in billItems" :key="owner.id" :owner="owner" />
 </template>
 
 <style scoped></style>
--- src/components/BillComp.vue
+++ src/components/BillComp.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
-import AddButton from "./AddButton.vue";
 import AddItem from "./AddItem.vue";
 import Item from "./Item.vue";
 import Total from "./Total.vue";
+
+const props = defineProps<{
+  owner: {
+    id: string;
+    name: string;
+    item: {
+      id: string;
+      price: number;
+    };
+  };
+}>();
 </script>
 
 <template>
-  <div class="flex flex-col gap-2 m-2">
+  <div class="flex flex-col gap-2 m-2 mb-6 p-2">
     <div class="flex gap-2 items-center">
-      <h2 class="font-bold">Jose</h2>
-      <AddButton />
+      <h2 class="font-bold">{{ props.owner.name }}</h2>
     </div>
     <Item />
     <AddItem />
     <hr class="border-ctp-text/10" />
     <Total />
   </div>
+  <hr class="border-ctp-text/30" />
 </template>
