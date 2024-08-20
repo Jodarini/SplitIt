fa2e906f09ca8ffece91c3fa7f503d1f85a13019
jodarini <rivasjose9428@gmail.com>
2024-08-20 15:40:18 -0500

    chore: empty store

--- src/store.ts
+++ src/store.ts
@@ -12,56 +12,7 @@ interface Bill {
 }
 
 export const store = reactive({
-  bills: <Bill[]>[
-    {
-      id: crypto.randomUUID(),
-      name: "Jose",
-      items: [
-        {
-          id: crypto.randomUUID(),
-          name: "Perro",
-          price: 3000,
-        },
-        {
-          id: crypto.randomUUID(),
-          name: "coca",
-          price: 2000,
-        },
-      ],
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Jessica",
-      items: [
-        {
-          id: crypto.randomUUID(),
-          name: "papas",
-          price: 3000,
-        },
-        {
-          id: crypto.randomUUID(),
-          name: "pepsi",
-          price: 4000,
-        },
-      ],
-    },
-    {
-      id: crypto.randomUUID(),
-      name: "Alguien",
-      items: [
-        {
-          id: crypto.randomUUID(),
-          name: "pizza",
-          price: 1000,
-        },
-        {
-          id: crypto.randomUUID(),
-          name: "agua",
-          price: 2000,
-        },
-      ],
-    },
-  ],
+  bills: <Bill[]>[],
   tax: 8,
   tip: 15,
 });
